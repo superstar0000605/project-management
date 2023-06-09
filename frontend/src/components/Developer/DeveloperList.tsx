bdf4ec54ed385080588ae80f82556f0bb2f59690
Lenoid Kovalenko <fangsong13@hotmail.com>
2023-06-09 15:46:44 +0300

    implement router and make simple components

---
+++ frontend/src/components/Developer/DeveloperList.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Button, Alert, Space, message } from 'antd';
+import {
+  UsergroupAddOutlined,
+} from '@ant-design/icons';
+import { fetchDevelopersAsync, createDeveloperAsync, updateDeveloperAsync, deleteDeveloperAsync, selectDevelopers, selectDevelopersStatus, selectDevelopersError } from '../../features/developerSlice';
+import { DeveloperType, DeveloperTableType } from '../../global.type';
+import { useAppDispatch } from '../../store';
+import DeveloperFormModal from './DeveloperFormModal';
+import DeveloperTable from './DeveloperTable';
+
+const DeveloperList: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [editingId, setEditingId] = useState('');
+  const [developer, setDeveloper] = useState<DeveloperType>({
+    name: '',
+    email: ''
+  })
+  const dispatch = useAppDispatch();
+  const developers = useSelector(selectDevelopers);
+  const developersStatus = useSelector(selectDevelopersStatus);
+  const developersError = useSelector(selectDevelopersError);
+  const [data, setData] = useState<DeveloperTableType[]>([]);
+  // setData(originData);
+  useEffect(() => {
+    dispatch(fetchDevelopersAsync());
+  }, [])
+
+  useEffect(() => {
+    let originData: DeveloperTableType[] = []
+    developers.map((developer) => {
+      originData.push({
+        key: developer.id,
+        name: developer.name,
+        email: developer.email,
+        projects: developer.projects?.reduce((result, project) => result += `${project.title}, `, '').slice(0, -2),
+      });
+      return 0;
+    });
+    setData(originData);
+  }, [developers]);
+
+  // useEffect(() => {
+  //   if (developersError) {
+  //     if (developersStatus === "failed") {
+  //       alert(developersError)
+  //     }
+  //   }
+  // }, [developersStatus])
+  const opnCreateDeveloperModal = () => {
+    setIsModalOpen(true);
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setDeveloper({
+      ...developer,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleCreateDeveloper = async (e: any) => {
+    const response = await dispatch(createDeveloperAsync(developer));
+    if (response.payload) setIsModalOpen(false);
+    else alert(developersError);
+  }
+  const handleDeleteButtonClick = (id: string) => {
+    dispatch(deleteDeveloperAsync(id))
+  }
+  const handleUpdateDeveloper = async (newDeveloper: { developerId: string, developer: DeveloperType }) => {
+    const response = await dispatch(updateDeveloperAsync(newDeveloper))
+    if (response.payload) setEditingId('');
+    else alert(developersError);
+  }
+  return (
+    <>
+      <Button style={{ marginBottom: "20px" }} icon={<UsergroupAddOutlined />} onClick={() => { opnCreateDeveloperModal(); }}>Create New developer</Button>
+      <DeveloperFormModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        handleChange={handleChange}
+        handleCreateDeveloper={handleCreateDeveloper}
+      />
+      <DeveloperTable
+        form={form}
+        data={data}
+        editingId={editingId}
+        handleDeleteButtonClick={handleDeleteButtonClick}
+        handleUpdateDeveloper={handleUpdateDeveloper}
+        setEditingId={setEditingId}
+      />
+    </>
+  );
+};
+
+export default DeveloperList;
---
+++ frontend/src/components/Project/ProjectList.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Button } from 'antd';
+import {
+  UsergroupAddOutlined,
+  FileAddOutlined,
+} from '@ant-design/icons';
+import { fetchProjectsAsync, createProjectAsync, updateProjectAsync, deleteProjectAsync, assignDevelopersAsync, selectProjects, selectProjectsStatus, selectProjectsError } from '../../features/projectSlice';
+import { fetchDevelopersAsync, selectDevelopers } from '../../features/developerSlice';
+import { ProjectTableType, ProjectType, DeveloperTableType } from '../../global.type';
+import { useAppDispatch } from '../../store';
+import ProjectFormModal from './ProjectFormModal';
+import ProjectTable from './ProjectTable';
+import AssigningDevelopersModal from './AssigningDevelopersModal';
+
+const ProjectList: React.FC = () => {
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingId, setEditingId] = useState('');
+  const [project, setProject] = useState<ProjectType>({
+    title: '',
+    description: ''
+  });
+  const [isViewAssigningModal, setIsViewAssigningModal] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const projects = useSelector(selectProjects);
+  const developers = useSelector(selectDevelopers);
+  const projectsStatus = useSelector(selectProjectsStatus);
+  const projectsError = useSelector(selectProjectsError);
+  const [data, setData] = useState<ProjectTableType[]>([]);
+  const [developersData, setDevelopersData] = useState<DeveloperTableType[]>([]);
+  const [projectId, setProjectId] = useState<string>('');
+
+  useEffect(() => {
+    dispatch(fetchProjectsAsync());
+  }, [])
+  // useEffect(() => {
+  //   if (projectsError) {
+  //     if (projectsStatus === "failed") {
+  //       alert(projectsError)
+  //     }
+  //   }
+  // }, [projectsStatus])
+  useEffect(() => {
+    let originData: ProjectTableType[] = [];
+    if (projects.length > 0) {
+      projects.map((project) => {
+        originData.push({
+          key: project.id,
+          title: project.title,
+          description: project.description,
+          developers: project.developers?.reduce((result, developer) => result += `${developer.name} (${developer.email}), ` , '').slice(0, -2),
+        });
+        return 0;
+      });
+      setData(originData);
+    }
+    let originDeveloperData: DeveloperTableType[] = [];
+    if (developers.length > 0) {
+      developers.map((developer) => {
+        originDeveloperData.push({
+          key: developer.id,
+          name: developer.name,
+          email:developer.email,
+          projects: '',
+        });
+        return 0;
+      });
+      setDevelopersData(originDeveloperData);
+    }
+  }, [projects, developers]);
+
+
+  const opnCreateProjectModal = () => {
+    setIsModalOpen(true);
+  }
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value
+    });
+  }
+  const handleCreateProject = async (e: any) => {
+    e.preventDefault();
+    const response = await dispatch(createProjectAsync(project));
+    if (response.payload) setIsModalOpen(false);
+    else alert(projectsError);
+  }
+  const handleDeleteButtonClick = (id: string) => {
+    dispatch(deleteProjectAsync(id))
+  }
+  const handleUpdateProject = async ({projectId, project}: {projectId:string, project:ProjectType}) => {
+    const response = await dispatch(updateProjectAsync({projectId, project}))
+    console.log(response)
+    if (response.payload) setEditingId('');
+    else alert(projectsError);
+  }
+  const handleSelectDeveloper = (ids: string[]) => {
+    dispatch(assignDevelopersAsync({projectId: projectId, developerIds: ids}));
+  }
+  const handleAssignDevelopers = (id: string) => {
+    setProjectId(id);
+    dispatch(fetchDevelopersAsync())
+    setIsViewAssigningModal(true);
+  }
+  return (
+    <>
+      <Button style={{ marginBottom: "20px" }} icon={<FileAddOutlined />} onClick={() => { opnCreateProjectModal(); }}>Create New project</Button>
+      <ProjectFormModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        handleChange={handleChange}
+        handleCreateProject={handleCreateProject}
+      />
+      <AssigningDevelopersModal
+        data={developersData}
+        isViewAssigningModal={isViewAssigningModal}
+        setIsViewAssigningModal={setIsViewAssigningModal}
+        handleSelectDeveloper={handleSelectDeveloper}
+      />
+      <ProjectTable
+        form={form}
+        data={data}
+        editingId={editingId}
+        handleDeleteButtonClick={handleDeleteButtonClick}
+        setEditingId={setEditingId}
+        handleAssignDevelopers={handleAssignDevelopers}
+        handleUpdateProject={handleUpdateProject}
+      />
+    </>
+  );
+};
+
+export default ProjectList;
